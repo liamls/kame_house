c06cf8b2b6aa75c3264d45b162e83fd7025ddbec
Liam Le Strat <lestratliam@gmail.com>
2025-04-14 18:56:56 +0200

    clean code

--- src/script.js
+++ src/script.js
@@ -4,62 +4,44 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import waterVertexShader from './shaders/water/vertex.glsl'
 import waterFragmentShader from './shaders/water/fragment.glsl'
-import { cubeMapNode } from 'three/src/nodes/utils/CubeMapNode.js'
-// Theme toggle
+
+/**
+ * Setup de base
+ */
 const toggleDayButton = document.getElementById('toggleTheme')
 const toggleMusicButton = document.getElementById('toggleMusic')
 let isNight = false
 let muted = true
-/**
- * Base
- */
-// Debug
-const gui = new GUI({
-    width: 300
-})
-const debugObject = {}
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
 
-// Scene
+const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
-/**
- * Loaders
- */
+// Debug
+const gui = new GUI({ width: 300 })
+const debugObject = {
+    nearColor: '#0596ed',
+    farColor: '#b3d7fb'
+}
 
-
-
-// Texture loader
+// Canvas, textures et matériaux
 const textureLoader = new THREE.TextureLoader()
 const bakedDayTexture = textureLoader.load('baked-day.jpg')
 const bakedNightTexture = textureLoader.load('baked.jpg')
 bakedDayTexture.colorSpace = THREE.SRGBColorSpace
-bakedDayTexture.flipY = false
 bakedNightTexture.colorSpace = THREE.SRGBColorSpace
+bakedDayTexture.flipY = false
 bakedNightTexture.flipY = false
-
-// Material
 
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedDayTexture })
 
-// GLTF loader
+// GLTF Loader pour la scène
 const gltfLoader = new GLTFLoader()
-
 gltfLoader.load('kame.glb', (gltf) => {
-    gltf.scene.traverse((child) => {
-        child.material = bakedMaterial
-    })
+    gltf.scene.traverse((child) => child.material = bakedMaterial)
     scene.add(gltf.scene)
 })
 
-/**
- * Object
- */
-
-debugObject.nearColor = '#0596ed'
-debugObject.farColor = '#b3d7fb'
-
+// Paramètres de l'eau
 const waterGeometry = new THREE.PlaneGeometry(120, 120, 512, 512)
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
@@ -74,92 +56,65 @@ const waterMaterial = new THREE.ShaderMaterial({
     },
     transparent: true,
     opacity: 0.95
-});
-
+})
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
 water.rotation.x = -Math.PI / 2
 water.position.y = 1.3
 scene.add(water)
 
-gui.addColor(debugObject, 'nearColor').onChange(() => { waterMaterial.uniforms.uColorNear.value.set(debugObject.nearColor) })
-gui.addColor(debugObject, 'farColor').onChange(() => { waterMaterial.uniforms.uColorFar.value.set(debugObject.farColor) })
+// Interface utilisateur pour le contrôle des couleurs de l'eau
+gui.addColor(debugObject, 'nearColor').onChange(() => waterMaterial.uniforms.uColorNear.value.set(debugObject.nearColor))
+gui.addColor(debugObject, 'farColor').onChange(() => waterMaterial.uniforms.uColorFar.value.set(debugObject.farColor))
 
-/** 
-* Sizes
+/**
+ * Configuration de la caméra
  */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-
-window.addEventListener('resize', () => {
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000)
-camera.position.x = -2
-camera.position.y = 12
-camera.position.z = 18
+camera.position.set(-2, 12, 18)
 scene.add(camera)
 
-// Controls
+// Contrôles
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.minPolarAngle = Math.PI / 3
 controls.maxPolarAngle = Math.PI / 3
-controls.enableZoom = false;
-controls.enablePan = false;
+controls.enableZoom = false
+controls.enablePan = false
+
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/**
+ * Musique et sons
+ */
 const listener = new THREE.AudioListener()
 camera.add(listener)
+
 const soundDay = new THREE.Audio(listener)
 const soundNight = new THREE.Audio(listener)
-soundDay.setLoop(true)
-soundNight.setLoop(true)
+
 const audioLoader = new THREE.AudioLoader()
-audioLoader.load('music.mp3', (buffer) => {
-    soundDay.setBuffer(buffer)
-    soundDay.setLoop(true)
-    soundDay.setVolume(0.5)
-})
-audioLoader.load('ambient.mp3', (buffer) => {
-    soundNight.setBuffer(buffer)
-    soundNight.setLoop(true)
-    soundNight.setVolume(0.5)
-})
+audioLoader.load('music.mp3', (buffer) => soundDay.setBuffer(buffer).setLoop(true).setVolume(0.5))
+audioLoader.load('ambient.mp3', (buffer) => soundNight.setBuffer(buffer).setLoop(true).setVolume(0.5))
+
 /**
- * Animate
+ * Animation
  */
 const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-    waterMaterial.uniforms.uTime.value = elapsedTime;
-    // Update controls
+    waterMaterial.uniforms.uTime.value = elapsedTime
     controls.update()
-
-    // Render
     renderer.render(scene, camera)
-
-    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
@@ -167,22 +122,36 @@ tick()
 
 gui.hide()
 
-toggleDayButton.addEventListener('click', () => {
-    isNight = !isNight
-    toggleDayButton.textContent = isNight ? '🌙' : '🌞'
+/**
+ * Fonction de changement de thème (jour/nuit) et musique
+ */
+const updateThemeAndMusic = () => {
+    // Changement de thème
     bakedMaterial.map = isNight ? bakedNightTexture : bakedDayTexture
     debugObject.nearColor = isNight ? '#046dac' : '#0596ed'
     debugObject.farColor = isNight ? '#304270' : '#b3d7fb'
     waterMaterial.uniforms.uColorNear.value.set(debugObject.nearColor)
     waterMaterial.uniforms.uColorFar.value.set(debugObject.farColor)
     bakedMaterial.needsUpdate = true
-    if (isNight) {
-        if (soundDay.isPlaying) soundDay.stop()
-        if (!soundNight.isPlaying) soundNight.play()
-    } else {
-        if (soundNight.isPlaying) soundNight.stop()
-        if (!soundDay.isPlaying) soundDay.play()
+    // Gestion des sons
+    if (!muted) {
+        if (isNight) {
+            soundDay.stop()
+            if (!soundNight.isPlaying) soundNight.play()
+        } else {
+            soundNight.stop()
+            if (!soundDay.isPlaying) soundDay.play()
+        }
     }
+}
+
+/**
+ * Gestion des événements
+ */
+toggleDayButton.addEventListener('click', () => {
+    isNight = !isNight
+    toggleDayButton.textContent = isNight ? '🌙' : '🌞'
+    updateThemeAndMusic()
 })
 
 toggleMusicButton.addEventListener('click', () => {
@@ -191,13 +160,17 @@ toggleMusicButton.addEventListener('click', () => {
     if (muted) {
         soundDay.stop()
         soundNight.stop()
-        return;
-    }
-    if (isNight) {
-        if (soundDay.isPlaying) soundDay.stop()
-        if (!soundNight.isPlaying) soundNight.play()
     } else {
-        if (soundNight.isPlaying) soundNight.stop()
-        if (!soundDay.isPlaying) soundDay.play()
+        updateThemeAndMusic()
     }
+})
+
+// Gestion du redimensionnement de la fenêtre
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
